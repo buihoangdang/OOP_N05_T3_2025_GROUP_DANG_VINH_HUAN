730ffca9286a3c0e8201f16be913a4b126f93318
Vinh210 <Vinhnguyen21022006@gmail.com>
2025-07-09 06:13:06 +0000

    commit

--- access/FullQualification.java
+++ access/FullQualification.java
@@ -1,9 +1,5 @@
-package access;
-public class FullQualification{
-    public static void main(String[] args) {
-        java.until.ArrayList list = new java.util.ArrayList();
-    
-
-    }
-
+public class FullQualification {
+ public static void main(String[] args) {
+ java.util.ArrayList list = new java.util.ArrayList();
+ }
 }
--- src/Time.java
+++ src/Time.java
@@ -1,10 +1,10 @@
 public class Time {
     // Method to convert seconds to hours, minutes, and seconds
-    public static String convertToHoursMinutesSeconds(int seconds) {
-        int hours = seconds / 3600;
-        int minutes = (seconds % 3600) / 60;
-        int remainingSeconds = seconds % 60;
+    public static String convertToHoursMinutesSeconds(int giay) {
+        int sogio = giay / 3600;
+        int sophut = (giay % 3600) / 60;
+        int sogiayconlai = giay % 60;
         
-        return hours + " hours, " + minutes + " minutes, " + remainingSeconds + " seconds";
+        return sogio + " gio, " + sophut + " phut, " + sogiayconlai + " giay";
     }
 }
