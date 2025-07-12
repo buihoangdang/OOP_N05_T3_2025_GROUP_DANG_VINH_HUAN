d94186b09688a38e8a1035d409a68b239ad7d7d2
Vinh210 <Vinhnguyen21022006@gmail.com>
2025-07-12 18:11:06 +0700

    gfj

--- test2/idk/src/Repository/CuaHangRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.CuaHang;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CuaHangRepository extends JpaRepository<CuaHang, String> {
-}
--- test2/idk/src/Repository/SanPhamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.SanPham;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SanPhamRepository extends JpaRepository<SanPham, String> {
-}
