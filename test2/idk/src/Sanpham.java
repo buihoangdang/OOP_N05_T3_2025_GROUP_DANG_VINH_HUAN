93912bb3b01c66de814f4479b6e53d7418c29057
buihoangdang <24106065@st.phenikaa-uni.edu.vn>
2025-07-15 18:23:46 +0700

    Update Sanpham.java

--- test2/idk/src/Sanpham.java
+++ test2/idk/src/Sanpham.java
@@ -1,64 +1,13 @@
-package com.example.demo.model;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "sanpham")
 public class SanPham {
-
-    @Id
-    private String id;
-    private String name;
-    private double price;
-    private int soluong;
-
-    public SanPham() {
-    }
+    public String id;
+    public String name;
+    public double price;
+    public int soluong;
 
     public SanPham(String id, String name, double price, int soluong) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.soluong = soluong;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public int getSoluong() {
-        return soluong;
-    }
-
-    public void setSoluong(int soluong) {
-        this.soluong = soluong;
-    }
-
-    public void hienThiThongTin() {
-        System.out.println("Sản phẩm: " + name);
-        System.out.println("Giá: " + price + " VND");
-        System.out.println("Số lượng còn: " + soluong);
     }
 }
