fd72ae7cd046ccbd3780d831a250a149911e7147
buihoangdang <24106065@st.phenikaa-uni.edu.vn>
2025-07-15 18:24:00 +0700

    Update Cuahang.java

--- test2/idk/src/Cuahang.java
+++ test2/idk/src/Cuahang.java
@@ -1,53 +1,11 @@
-package com.example.demo.model;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "cuahang")
 public class CuaHang {
-
-    @Id
-    private String id;
-    private String name;
-    private String location;
-
-    public CuaHang() {
-    }
+    public String id;
+    public String name;
+    public String location;
 
     public CuaHang(String id, String name, String location) {
         this.id = id;
         this.name = name;
         this.location = location;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public void hienThiThongTin() {
-        System.out.println("Cửa hàng: " + name);
-        System.out.println("Địa điểm: " + location);
     }
 }
