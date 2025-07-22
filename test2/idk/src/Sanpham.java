15746993fcb2700e70964a1e98fc25cf04d32a9d
buihoangdang <24106065@st.phenikaa-uni.edu.vn>
2025-07-22 16:41:33 +0700

    Update Sanpham.java

--- test2/idk/src/Sanpham.java
+++ test2/idk/src/Sanpham.java
@@ -3,11 +3,12 @@ public class SanPham {
     public String name;
     public double price;
     public int soluong;
-
-    public SanPham(String id, String name, double price, int soluong) {
+    public String idCuaHang; 
+    public SanPham(String id, String name, double price, int soluong, String idCuaHang) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.soluong = soluong;
+        this.idCuaHang = idCuaHang;
     }
 }
