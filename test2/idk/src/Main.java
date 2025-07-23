ff1c84aaa4f28e81ebb9323373790a9b98a44629
Vinh210 <Vinhnguyen21022006@gmail.com>
2025-07-23 07:40:34 +0000

    lambaistudent

--- test2/idk/src/Main.java
+++ test2/idk/src/Main.java
@@ -3,55 +3,61 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<SanPham> dsSP = new ArrayList<>();
-        ArrayList<KhachHang> dsKH = new ArrayList<>();
-        ArrayList<CuaHang> dsCH = new ArrayList<>();
+        // ArrayList<SanPham> dsSP = new ArrayList<>();
+        // ArrayList<KhachHang> dsKH = new ArrayList<>();
+        // ArrayList<CuaHang> dsCH = new ArrayList<>();
 
-        QuanLySanPham qlsp = new QuanLySanPham(dsSP);
-        QuanLyKhachHang qlkh = new QuanLyKhachHang(dsKH);
-        QuanLyCuaHang qlch = new QuanLyCuaHang(dsCH);
+        // QuanLySanPham qlsp = new QuanLySanPham(dsSP);
+        // QuanLyKhachHang qlkh = new QuanLyKhachHang(dsKH);
+        // QuanLyCuaHang qlch = new QuanLyCuaHang(dsCH);
 
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
 
-        System.out.println("===== SẢN PHẨM =====");
-        qlsp.Create(new SanPham("SP1", "Áo thun", 120000, 10, "CH1"));
-        qlsp.Create(new SanPham("SP2", "Quần jeans", 250000, 5, "CH2"));
-        qlsp.Print();
-        qlsp.Edit("SP1");
-        qlsp.Print();
-        qlsp.Delete("SP2");
-        qlsp.Print();
+        // System.out.println("===== SẢN PHẨM =====");
+        // qlsp.Create(new SanPham("SP1", "Áo thun", 120000, 10, "CH1"));
+        // qlsp.Create(new SanPham("SP2", "Quần jeans", 250000, 5, "CH2"));
+        // qlsp.Print();
+        // qlsp.Edit("SP1");
+        // qlsp.Print();
+        // qlsp.Delete("SP2");
+        // qlsp.Print();
 
-        System.out.println("\n===== KHÁCH HÀNG =====");
-        qlkh.Create(new KhachHang("KH1", "Nguyễn Văn A", "0901234567", "Hà Nội"));
-        qlkh.Create(new KhachHang("KH2", "Trần Thị B", "0987654321", "TP.HCM"));
-        qlkh.Print();
-        qlkh.Edit("KH2");
-        qlkh.Print();
-        qlkh.Delete("KH1");
-        qlkh.Print();
+        // System.out.println("\n===== KHÁCH HÀNG =====");
+        // qlkh.Create(new KhachHang("KH1", "Nguyễn Văn A", "0901234567", "Hà Nội"));
+        // qlkh.Create(new KhachHang("KH2", "Trần Thị B", "0987654321", "TP.HCM"));
+        // qlkh.Print();
+        // qlkh.Edit("KH2");
+        // qlkh.Print();
+        // qlkh.Delete("KH1");
+        // qlkh.Print();
 
-        System.out.println("\n===== CỬA HÀNG =====");
-        qlch.Create(new CuaHang("CH1", "Cửa hàng 1", "Hà Nội"));
-        qlch.Create(new CuaHang("CH2", "Cửa hàng 2", "Đà Nẵng"));
-        qlch.Print();
-        qlch.Edit("CH1");
-        qlch.Print();
-        qlch.Delete("CH2");
-        qlch.Print();
+        // System.out.println("\n===== CỬA HÀNG =====");
+        // qlch.Create(new CuaHang("CH1", "Cửa hàng 1", "Hà Nội"));
+        // qlch.Create(new CuaHang("CH2", "Cửa hàng 2", "Đà Nẵng"));
+        // qlch.Print();
+        // qlch.Edit("CH1");
+        // qlch.Print();
+        // qlch.Delete("CH2");
+        // qlch.Print();
 
    
 
-        System.out.println("\n===== Hiển thị sản phẩm theo mã cửa hàng =====");
-        System.out.print("Nhập mã cửa hàng: ");
-        String maCH = sc.nextLine();
-        qlsp.hienThiSanPhamTheoCuaHang(maCH);
+        // System.out.println("\n===== Hiển thị sản phẩm theo mã cửa hàng =====");
+        // System.out.print("Nhập mã cửa hàng: ");
+        // String maCH = sc.nextLine();
+        // qlsp.hienThiSanPhamTheoCuaHang(maCH);
 
-        System.out.println("\n===== Tìm kiếm khách hàng theo tên hoặc SĐT =====");
-        System.out.print("Nhập từ khóa tìm kiếm: ");
-        String tuKhoa = sc.nextLine();
-        qlkh.timKiemKhachHang(tuKhoa);
+        // System.out.println("\n===== Tìm kiếm khách hàng theo tên hoặc SĐT =====");
+        // System.out.print("Nhập từ khóa tìm kiếm: ");
+        // String tuKhoa = sc.nextLine();
+        // qlkh.timKiemKhachHang(tuKhoa);
 
-       
+       Student s1 = new Student("Fred", 3.0F);
+       Student s2 = new Student("Sam", 3.5F);
+       Student s3 = new Student("Steve", 2.1F);
+       System.out.println("So sánh s1 và s2: " + s1.compareTo(s2)); 
+       System.out.println("So sánh s2 và s1: " + s2.compareTo(s1)); 
+       System.out.println("So sánh s1 và s3: " + s1.compareTo(s3)); 
+
     }
 }
--- test2/idk/src/Student.java
+++ test2/idk/src/Student.java
@@ -10,4 +10,14 @@ public int compareTo(Object o) {
 if ( ((Student)o).gpa < gpa ) return 1;
 else if ( ((Student)o).gpa > gpa ) return -1;
 else return 0;
-} 
+}
+public boolean equals(Object o) {
+//cast the correct type (Student)o
+if (gpa == ((Student) o).gpa) return true;
+else return false;
+}
+public String getName() { return name;}
+public float getGpa() { return gpa;}
+private String name;
+private float gpa;
+}
