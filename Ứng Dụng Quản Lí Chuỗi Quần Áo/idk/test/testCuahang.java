04928dc52693fbf7d7b93c6538ad1883ebefa0ad
buihoangdang <24106065@st.phenikaa-uni.edu.vn>
2025-08-10 21:30:08 +0700

    remake project

--- Ứng Dụng Quản Lí Chuỗi Quần Áo/idk/test/testCuahang.java
+++
@@ -1,6 +0,0 @@
-public class testCuahang {
-    public static void main(String[] args) {
-        CuaHang ch = new CuaHang("CH01", "Cửa hàng Quận 1", "123 Lê Lợi, Quận 1");
-        ch.hienThiThongTin();
-    }
-}
--- Ứng Dụng Quản Lí Chuỗi Quần Áo/idk/test/testSanpham.java
+++
@@ -1,6 +0,0 @@
-public class testSanpham {
-    public static void main(String[] args) {
-        SanPham sp = new SanPham("SP01", "Áo thun", 199000, 10);
-        sp.hienThiThongTin();
-    }
-}
