9fa5c3a0cad59b5d0e8718a3a6f2d25c5c30e829
Vinh210 <Vinhnguyen21022006@gmail.com>
2025-08-10 22:54:49 +0700

    debug main function

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -1,14 +1,13 @@
 package com.mycompany.mavenproject1;
 
-import com.mycompany.mavenproject1.view.MainView;
+import com.mycompany.mavenproject1.view.LoginView;
 
 public class Mavenproject1 {
 
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(() -> {
-            MainView view = new MainView();
+            LoginView view = new LoginView();
             view.setVisible(true);
         });
     }
-
 }
