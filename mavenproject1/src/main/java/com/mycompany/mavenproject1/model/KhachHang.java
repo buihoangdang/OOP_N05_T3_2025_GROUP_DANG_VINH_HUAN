784e18bb31bd34b28d5315f19c0cfcd226791a58
buihoangdang <24106065@st.phenikaa-uni.edu.vn>
2025-08-11 12:56:01 +0700

    KhachHang.java

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/model/KhachHang.java
@@ -0,0 +1,50 @@
+package com.mycompany.mavenproject1.model;
+
+public class KhachHang {
+    private String maKhachHang;
+    private String hoTen;
+    private String soDienThoai;
+    private String diaChi;
+
+    public KhachHang() {
+    }
+
+    public KhachHang(String maKhachHang, String hoTen, String soDienThoai, String diaChi) {
+        this.maKhachHang = maKhachHang;
+        this.hoTen = hoTen;
+        this.soDienThoai = soDienThoai;
+        this.diaChi = diaChi;
+    }
+
+    public String getMaKhachHang() {
+        return maKhachHang;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public String getSoDienThoai() {
+        return soDienThoai;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setMaKhachHang(String maKhachHang) {
+        this.maKhachHang = maKhachHang;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public void setSoDienThoai(String soDienThoai) {
+        this.soDienThoai = soDienThoai;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }    
+}
